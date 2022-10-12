e7d53ca4f013ec5764f8a3c5d021279407292fc4
shovan <shovonmahmood67@gmail.com>
2022-10-12 14:21:44 +0600

    add list components

---
+++ IMS.WebApp/Pages/Inventories/InventoryListBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace IMS.WebApp.Pages.Inventories
+{
+    public class InventoryListBase : ComponentBase
+    {
+        protected string SearchTerm { get; private set; } = string.Empty;
+        protected void OnSearchInventory(string searchTerm)
+        {
+            SearchTerm = searchTerm;
+        }
+    }
+}
